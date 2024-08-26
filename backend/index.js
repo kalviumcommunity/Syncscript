8eed3e381dc63112ba2fd216c8335e709651b86f
Dwij13 <dwijjindal2005@gmail.com>
2024-08-26 14:42:19 +0530

    Established mongodb connection and created schema

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const MongoConnection = require("./config/db")
 
 dotenv.config()
 
@@ -13,6 +14,8 @@ app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
 }))
+
+MongoConnection();  // connection to the server
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
