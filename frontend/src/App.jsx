53d143d7b86425cae23b917657fc1c25df63345f
Aditya Pourush <adityapourush11@outlook.com>
2024-11-25 11:23:01 +0530

    improvements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Router, Route, Routes, Navigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
-import AuthPage from "./pages/Register/AuthPage";
-import userAtom from "./atoms/userAtoms";
-import HomePage from "./pages/Dashboard/HomePage";
 function App() {
-  const user = useRecoilValue(userAtom);
 
   return (
     <div className="App">
-      <Routes>
-        <Route
-          path="/"
-          element={user ? <HomePage /> : <Navigate to="/auth" />}
-        />
-        <Route
-          path="/auth"
-          element={!user ? <AuthPage /> : <Navigate to="/" />}
-        />
-      </Routes>
+      
     </div>
   );
 }
